9e466ccbf8cf346993583977592c4dde01718830
orelhadeburro <61843878+orelhadeburro@users.noreply.github.com>
2023-11-30 09:09:37 -0300

    subi os arquivos

---
+++ teste.php
@@ -0,0 +1,218 @@
+<?php
+echo" 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'/>
+<title>GH iPhones & Acessórios</title>
+<link rel='stylesheet' type='text/css' href='funciona.css' />
+</head>
+
+<body>
+<div id='topo'></div>
+<div id='tudo'>
+<h1><font color='white'>Tudo em iPhones pra você</font></h1>
+<h4>Faça seu orçamento conosco e ganhe frete grátis!</h4>
+<p><font color='blue'><b>iPhones <br>lacrados!</font><br>
+O melhor<br> preço do <br>Tocantins</b></p>
+
+<div id='opcao'>
+<img src='fundo.avif' id='fundoimg1'>
+<img src='frete.avif' id='frete'>
+<img src='iphone.avif' id='iphone'>
+<img src='garantia.jpg' id='garantia'>
+<h2 id='textfrete'><font color='white'>&nbsp&nbsp&nbsp&nbspFrete Grátis<br>Todo o Tocantins</font></h2>
+<h2 id='textiphone'><font color='white'>+550 iPhones<br>&nbsp&nbsp&nbspEntregues</font></h2>
+<h2 id='textgarantia'><font color='white'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1 Ano de<br>Garantia Apple</font></h2>
+</div>
+
+<p id='veja'><font color='#051B43'>Veja os principais<br> produtos <b>Apple</b></font><p>
+<img src='iphone11preto.jpg' id='iphone11preto'>
+<img src='iphone12roxo.jpg' id='iphone12roxo'>
+<img src='iphone 13.webp' id='iphone13verde'>
+<img src='iphone14azul.png' id='iphone14azul'>
+<img src='iphone15.jpg' id='iphone14pro'>
+<img src='iphone15pro.jpg' id='iphone14promax'>
+<img src='iphone15pro.jpg' id='iphone15promax'>
+<img src='ipad.jpg' id='ipad'>
+<img src='watch.jpg' id='watch'>
+<img src='airpods.jpg' id='airpods'>
+<img src='fonte.webp' id='fonte'>
+<img src='cabo.webp' id='cabo'>
+
+
+
+
+<img src='preto11.avif' id='preto11'>
+<img src='branco11.jpg' id='branco11'>
+<img src='amarelo11.jpg' id='amarelo11'>
+<img src='azul11.avif' id='azul11'>
+<img src='vermelho11.jpg' id='vermelho11'>
+
+
+<img src='preto12.avif' id='preto12'>
+<img src='branco12.jpg' id='branco12'>
+<img src='azul12.webp' id='azul12'>
+<img src='roxo12.jpg' id='roxo12'>
+<img src='vermelho12.jpg' id='vermelho12'>
+
+
+<img src='preto13.avif' id='preto13'>
+<img src='branco13.jpg' id='branco13'>
+<img src='azul13.webp' id='azul13'>
+<img src='rosa13.jpg' id='rosa13'>
+<img src='verde13.webp' id='verde13'>
+
+<img src='preto13.avif' id='preto14'>
+<img src='branco13.jpg' id='branco14'>  
+<img src='roxo12.jpg' id='roxo14'>
+<img src='azul11.avif' id='azul14'>
+<img src='vermelho12.jpg' id='vermelho14'>
+
+<img src='rosa13.jpg' id='preto14pro'>
+<img src='branco13.jpg' id='branco14pro'> 
+<img src='azul11.avif' id='roxo14pro'>
+<img src='amarelo11.jpg' id='amarelo14pro'>
+
+<img src='preto13.avif' id='preto14promax'>
+<img src='branco13.jpg' id='branco14promax'> 
+<img src='cinza.avif' id='roxo14promax'>
+<img src='azultitanium.avif' id='amarelo14promax'>
+
+<img src='preto13.avif' id='preto15promax'>
+<img src='branco13.jpg' id='branco15promax'> 
+<img src='cinza.avif' id='cinza15promax'>
+<img src='azultitanium.avif' id='azul15promax'>
+
+<img src='cinza.avif' id='cinzaipad'>
+<img src='branco13.jpg' id='brancoipad'> 
+
+<img src='branco13.jpg' id='brancowatch'> 
+<img src='preto13.avif' id='pretowatch'>
+
+
+
+<p id='legenda11'>iPhone 11</p>
+<p id='legenda12'>iPhone 12</p>
+<p id='legenda13'>iPhone 13</p>
+<p id='legenda14'>iPhone 14</p>
+<p id='legenda15'>iPhone 15</p>
+<p id='legenda14promax'>iPhone 15 PRO</p>
+<p id='legenda15promax'>iPhone 15 PRO MAX</p>
+<p id='legendaipad'>iPad</p>
+<p id='legendawatch'>Apple Watch</p>
+<p id='legendaair'>Air Pods</p>
+<p id='legendafonte'>Fonte USB-c 20w</p>
+<p id='legendacabo'>Cabo USB-c</p>
+
+
+<br>
+<a href='https://wa.me/5563984328186?text=Ol%C3%A1%20desejo%20fazer%20or%C3%A7amento%20do%20iPhone%2011'class='button button' value='iPhone 11'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+12&type=phone_number&app_absent=0'class='button button1' value='iPhone 12'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+13&type=phone_number&app_absent=0'class='button button2' value='iPhone 13'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+14&type=phone_number&app_absent=0'class='button button3' value='iPhone 14'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+15&type=phone_number&app_absent=0'class='button button4' value='iPhone 15'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+15+PRO&type=phone_number&app_absent=0'class='button button5' value='iPhone 15 PRO '>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPhone+15+PRO+MAX&type=phone_number&app_absent=0'class='button button6' value='iPhone 15 PRO MAX'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+iPad&type=phone_number&app_absent=0'class='button button7' value='IPAD'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+Smart+Watch&type=phone_number&app_absent=0'class='button button8' value='SMART WATCH'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+Air+Pods&type=phone_number&app_absent=0'class='button button9' value='AIR PODS'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+da+Fonte+20w&type=phone_number&app_absent=0'class='button button10' value='Fonte'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+Cabo+USB-C&type=phone_number&app_absent=0'class='button button11' value='cabo'>Faça seu Orçamento</a>
+
+<p id='vejajbl'>Veja os principais<br>&nbsp&nbsp&nbsp&nbsp produtos <b>JBL</b></p>
+<img src='bombox3.jpg' id='bombox'>
+<img src='jbl520.jpg' id='jbl520'>
+<img src='jbl760.jpg' id='jbl760'>
+
+
+
+<p id='legendabombox'>Boombox 3</p>
+<p id='legenda520'>Fone JBL 520bt</p>
+<p id='legenda760'>Fone JBL 760nc</p>
+
+
+
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+da+Boombox+3&type=phone_number&app_absent=0'class='button button12' value='cabo'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+fone+JBL+520bt&type=phone_number&app_absent=0'class='button button13' value='cabo'>Faça seu Orçamento</a>
+<a href='https://api.whatsapp.com/send/?phone=5563984328186&text=Ol%C3%A1+desejo+fazer+or%C3%A7amento+do+fone+JBL+760nc&type=phone_number&app_absent=0'class='button button14' value='cabo'>Faça seu Orçamento</a>
+
+
+
+
+
+
+
+<img src='fundo.avif' id='fundoimg2'>
+
+<img src='branco.png' id='fundobranco'>
+<img src='branco.png' id='fundobranco1'>
+
+<p id='parcela'>&nbsp&nbsp&nbsp&nbsp&nbspParcele em ate 18x no seu <br>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspCartão de Crédito🤩💰</p>
+<p id='descricao'>Faça o orçamento e receba as melhores ofertas <br>&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsppara adquirir o seu iPhone novo.</p>
+
+<p id='descricao1'>&nbsp&nbspFrete Grátis para <br>Todo o Tocantins!🚛</p>
+<p id='descricao2'>Faça o orçamento e receba as melhores ofertas <br>&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsppara adquirir o seu iPhone novo.</p>
+
+<div='foto'><img src='GH1.png' id='loja'></div>
+
+
+
+
+<p id='equipe'><font color='#051B43'><b><u>Equipe GH iPhones e Acessórios</u></b></font></p>
+
+
+
+<img src='jadilson.webp' id='jadilson'>
+<h3>Jadilson Reis</h3>
+<h2>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbspGerente <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspGH iPhones e Acessórios</h2>
+<p id='conheca'>&nbspConheça a <br> GH iPhones<br> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp& <br>&nbspAcessórios </p>
+
+<img src='jadilson.webp' id='jadilson1'>
+<h3 id='nometecnico'>Lucas Henrique</h3>
+<h2 id='tecnico'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+Técnico <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspGH iPhones e Acessórios</h2>
+
+<img src='jadilson.webp' id='jadilson2'>
+<h3 id='nomevendedor'>Thyago Araujo</h3>
+<h2 id='vendedor'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+Vendedor <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp GH iPhones e Acessórios</h2>
+
+<p id='text'><b>SOMOS A GH IPHONES E ACESSÓRIOS.<br> UMA EMPRESA COM MAIS DE 5 ANOS<br> NO MERCARDO,
+ BUSCANDO SEMPRE <br>ENTREGAR AO CLIENTE A MELHOR <br>EXPERIÊNCIA POSSÍVEL COM OS<br> PRODUTOS DA 
+ LINHA APPLE E JBL<br><br>
+
+#VEMPRAGH<br><br></b>
+
+<i>Porto Nacional - Tocantins<br>
+GH iPhones e Acessórios</i>
+</p>
+
+
+<div id='rodape'> <p id='criado'>COPYRIGHT© Thyago Araujo - 2023</p></div>
+
+</div>
+</body>
+</body>
+
+</html>
+"
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
